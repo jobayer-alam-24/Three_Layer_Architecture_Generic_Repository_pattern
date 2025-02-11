270d0becca67f9d2349c798ca3724d3f589da0e3
Jobayer Alam <sheikhnayamul123@gmail.com>
2025-02-11 21:18:41 +0600

    done

--- ThreeLayerArchitecture/ThreeLayerArchitecture/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ThreeLayerArchitecture.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
